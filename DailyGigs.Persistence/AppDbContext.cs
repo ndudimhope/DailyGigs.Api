999e87cbf11a10d16b38c76d9f5e1a3ac9a5f452
Hope Ndudim <59125945+ndudimhope@users.noreply.github.com>
2021-08-22 18:19:16 +0100

    Configured AppDbContext

---
+++ DailyGigs.Persistence/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using DailyGigs.Entity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyGigs.Persistence
+{
+   public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hustler> Hustlers { get; set; }
+        public DbSet<Plug> Plugs { get; set; }
+        public DbSet<Gig> Gig { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //seed data into Category
+            modelBuilder.Entity<Category>().HasData(new Category("Events"));
+            modelBuilder.Entity<Category>().HasData(new Category("Arts/Crafts"));
+            modelBuilder.Entity<Category>().HasData(new Category("Social Media"));
+            modelBuilder.Entity<Category>().HasData(new Category("Content Creation"));
+
+
+           
+
+        }
+
+    }
+}
---
+++ DailyGigs.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DailyGigs.Persistence
+{
+    public class Class1
+    {
+    }
+}
